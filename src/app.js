89296427777c490a7df7c77f4f89f255fe32cd2e
Mike Shine <michaelbshine@outlook.com>
2021-02-26 08:18:26 -0800

    adding webpack

--- src/app.js
+++ src/app.js
@@ -1,3 +1,7 @@
+import {apiKey} from '../apiKey.js';
+
+console.log(apiKey);
+
 // write functions that hit API
   // should be able to take a location (city) and return weather data for that location
   // just console.log for now
