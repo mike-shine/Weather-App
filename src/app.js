b174b62d64754f4e918a678ae300e31e47486f33
Mike Shine <michaelbshine@outlook.com>
2021-02-26 14:01:43 -0800

    function now returns relevant weather data in object form. also removed babel dev dependencies

--- src/app.js
+++ src/app.js
@@ -5,13 +5,12 @@ async function getWeather() {
     let city = prompt('Please enter a city');
     let response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`, {mode: 'cors'});
     let weatherData = await response.json();
-    console.log(weatherData);
-    // .then(function(response) {
-    //   return response.json();
-    // })
-    // .then(function(res) {
-    //   console.log(res);
-    // })
+    let relevantWeatherData = {
+      location: weatherData.name,
+      temperature: weatherData.main.temp,
+      weather: weatherData.weather[0].description
+    }
+    console.table(relevantWeatherData);
 };
 
 
@@ -19,9 +18,6 @@ window.addEventListener('DOMContentLoaded', () => {
   getWeather();
 });
 
-// write functions that hit API
-  // should be able to take a location (city) and return weather data for that location
-  // just console.log for now
 
     // .then(function(response) {
   //   return response.json();
